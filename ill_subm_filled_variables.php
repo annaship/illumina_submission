09e06545fcc6101bccd1fd02d17833e17e9067a4
annaship <tetya-loshad@yandex.ru>
2013-02-21 11:28:01 -0500

    temp

--- ill_subm_filled_variables.php
+++ ill_subm_filled_variables.php
@@ -5,40 +5,20 @@ $arr_fields_headers = array("domain", "lane", "data_owner", "run_key", "barcode_
 
 // ---
 
-$query = "SELECT DISTINCT user, passwd, first_name, last_name, active, security_level, email, institution, id, date_added
+$query = "SELECT DISTINCT user, first_name, last_name, active, security_level, email, institution, id, date_added
             FROM vamps.vamps_auth ORDER BY last_name";
 $result_vamps_user = mysql_query($query, $vampsprod_connection) or die("SELECT Error: $result_vamps_user: ".mysql_error());
 $i = 0;
 while($row = mysql_fetch_row($result_vamps_user))
 {
   $i += 1;
-  $contact[$i]      = $row[3].', '.$row[2];
-  $contact_full[$i] = $row[3].', '.$row[2].', '.$row[6].', '.$row[7]; 
+  $contact[$i]      = $row[2].', '.$row[1];
+  $contact_full[$i] = $row[2].', '.$row[1].', '.$row[5].', '.$row[6]; 
 }
 print "<br/>contact: <br/>";
 print_r($contact); 
 print "<br/>contact: <br/>";
 print_r($contact_full);
-
-// ---
-// $query = "SELECT DISTINCT first_name, last_name FROM env454.contact WHERE last_name <> '' ORDER BY last_name";
-// $result_contact = mysql_query($query, $newbpc2_connection) or die("SELECT Error: $result_contact: ".mysql_error());
-// $i = 0;
-// while($row = mysql_fetch_row($result_contact))
-// {
-//   $i += 1;
-//   $contact[$i] = $row[1].', '.$row[0];
-// }
-// // ---
-
-// $query = "SELECT DISTINCT last_name, first_name, email, institution FROM env454.contact WHERE last_name <> '' ORDER BY last_name";
-// $result_contact_full = mysql_query($query, $newbpc2_connection) or die("SELECT Error: $result_contact_full: ".mysql_error());
-// $i = 0;
-// while($row = mysql_fetch_row($result_contact_full))
-// {
-//   $i += 1;
-//   $contact_full[$i] = $row[0].', '.$row[1].', '.$row[2].', '.$row[3];
-// }
 
 // ---
 $query = "SELECT DISTINCT project FROM env454.project WHERE project <> '' ORDER BY project";
