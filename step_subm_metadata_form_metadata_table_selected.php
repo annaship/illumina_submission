5645ea17ad10628cd989efd85acfa88205623348
annaship <tetya-loshad@yandex.ru>
2013-03-20 13:18:50 -0400

    prints a table with selected values, intermediate

---
+++ step_subm_metadata_form_metadata_table_selected.php
@@ -0,0 +1,46 @@
+<!-- <form action='step_subm_metadata_form_metadata_table_submit.php' method='post'> -->
+<form method="post" name="submission_metadata_form" id="submission_metadata_form" action="step_subm_metadata.php">
+<div id="submission_metadata">
+  <fieldset id="submission_metadata">
+    <legend id="submission_metadata-legend">Submition metadata</legend>
+    <table id="submission_metadata-fields">
+      <thead>
+        <tr>
+          <!-- add auto complete -->
+          <?php 
+          
+          foreach ($arr_fields_headers as &$value) {
+            echo '
+            <th>'.$value.'</th>
+            ';  
+          
+          }
+          unset($value); // break the reference with the last element
+          
+          ?>      
+          </tr>
+        </thead>
+        <tbody>   
+          <?php
+              foreach ($selected_metadata_arr as $selected_metadata_arr1)
+              {
+//                 take only arrays with selected values
+                if(isset($selected_metadata_arr1["domain"]))
+                {
+                  foreach($selected_metadata_arr1 AS $key => $value)
+                  {
+                    ${"selected_" . $key} = $value;
+                  }
+                }
+                include("step_subm_metadata_form_metadata_table_rows.php");
+              }
+          ?>     
+        </tbody>
+    </table>
+<!--   <a href="#" title="" class="add-author">Add Author</a> -->
+  </fieldset>
+  <input type="submit" name="update" id="form_update" value="Update submission metadata"/> 
+  <input type="submit" name="cancel" id="form_cancel" value="Cancel"/> 
+  <input type="hidden" name="submission_metadata_process" value="2">
+</div>
+</form>
---
+++ step_subm_metadata_form_submission_metadata_validation.php
@@ -0,0 +1,42 @@
+<?php
+include_once "ill_subm_filled_variables.php";
+include_once("ill_subm_functions.php");
+$metadata_errors = array();
+$result_metadata_arr_checked = $selected_metadata_arr = array();
+$result_metadata_arr = separate_metadata($_POST, $arr_fields_headers);
+// print_out($result_metadata_arr);
+
+// TODO: 1) validate all data in foreach $result_metadata_arr
+// TODO: 2) populate index and runkey by adapter
+// TODO: 3) print out in table to show with errors in red and allow to change, 
+// TODO: 4) then validate again and submit to db 
+
+$required_fields = array();
+$i = 0;
+$required_fields = create_require_arr($submission_metadata_form_fields);
+// TODO: 1) validate all data in foreach $result_metadata_arr
+
+foreach ($result_metadata_arr as $result_metadata_arr1)
+{
+  $metadata_errors = check_required_fields($result_metadata_arr1, $required_fields);
+  $field_name = "lane";
+  if( isset($result_metadata_arr1[$field_name]) && !valid_is_number($result_metadata_arr1[$field_name]))
+  {
+    $metadata_errors[$field_name] = "The " . $field_name . " should be numbers.";
+  }
+//   print_out("\$result_metadata_arr1[\$field_name] = " . $result_metadata_arr1[$field_name]);
+//   print_out($metadata_errors);
+
+  // TODO: 2) populate index and runkey by adapter  
+  $selected_dna_region_base = $_POST["selected_dna_region_base"];
+  
+  $key_ind = get_run_key_by_adaptor($result_metadata_arr1, $adaptors_full, $selected_dna_region_base);
+  $result_metadata_arr1["run_key"]       = $key_ind["illumina_run_key"];
+  $result_metadata_arr1["barcode_index"] = $key_ind["illumina_index"];    
+  $selected_metadata_arr[] = $result_metadata_arr1;
+}
+
+// TODO: 3) print out in table to show with errors in red and allow to change,
+  include_once "step_subm_metadata_form_metadata_table_selected.php";
+?>
+
