9dd9c4255fab006b0e71073448a97a47634044c7
annaship <tetya-loshad@yandex.ru>
2013-03-21 12:36:25 -0400

    in table_rows; all green

--- step_subm_metadata_form_metadata_table_rows.php
+++ step_subm_metadata_form_metadata_table_rows.php
@@ -1,7 +1,7 @@
 <?php print_out($row_num);?>
       <tr class="odd">
         <td>
-          <select name="domain_0" id="form_domain_0">
+          <select name="domain_<?php echo $row_num;?>" id="form_domain_<?php echo $row_num;?>">
            <?php 
               $domain_wo_abbr = array("Bacteria" => "B", "Archaea" => "A", "Eukarya" => "E", "Fungi" => "F");
               foreach ($domain_wo_abbr as $full_name => $abbr)
@@ -19,12 +19,12 @@
           </select>                
         </td>
       
-        <td><input class="text_inp size_number" type="text" name="lane_0" id="form_lane_0" value="<?php echo $selected_lane ?>"/></td>
+        <td><input class="text_inp size_number" type="text" name="lane_<?php echo $row_num;?>" id="form_lane_<?php echo $row_num;?>" value="<?php echo $selected_lane ?>"/></td>
         
         <!-- Show Suite by dna_region plus domain -->     
         <td>
 
-        <select name="data_owner_0" id="form_data_owner_0">
+        <select name="data_owner_<?php echo $row_num;?>" id="form_data_owner_<?php echo $row_num;?>">
          <?php 
       //   TODO: how to chose if 2 the same name?
 //          $selected_data_owner = "Anderson, Rika";
@@ -39,13 +39,15 @@
         ?>
         </select></td>
       
-        <td>NNNN<input class="text_inp wide"               type="text" name="run_key_0"       id="form_run_key_0"       readonly="readonly" 
+        <td>NNNN<input class="text_inp wide"           type="text" name="run_key_<?php echo $row_num;?>"       
+        id="form_run_key_<?php echo $row_num;?>"       readonly="readonly" 
         value="<?php echo $selected_run_key ?>"/></td>
-        <td><input class="text_inp size_barcode_index" type="text" name="barcode_index_0" id="form_barcode_index_0" readonly="readonly" 
+        <td><input class="text_inp size_barcode_index" type="text" name="barcode_index_<?php echo $row_num;?>" 
+        id="form_barcode_index_<?php echo $row_num;?>" readonly="readonly" 
         value="<?php echo $selected_barcode_index ?>"/></td>
       
         <td>
-        <select name="adaptor_0" id="form_adaptor_0">
+        <select name="adaptor_<?php echo $row_num;?>" id="form_adaptor_<?php echo $row_num;?>">
       
        <?php
   		 print_options(array_values($adaptors), $selected_adaptor);    
@@ -53,18 +55,18 @@
         ?>  
         </select></td>
         <td>
-        <select name="project_0" id="form_project_0">
+        <select name="project_<?php echo $row_num;?>" id="form_project_<?php echo $row_num;?>">
       
        <?php
 			print_options($project, $selected_project);        
         ?>  
         </select></td>
       
-        <td><input class="text_inp size_dataset" type="text" name="dataset_0" id="form_dataset_0" value="<?php echo $selected_dataset ?>"/></td>
-        <td><input class="text_inp size_dataset" type="text" name="dataset_description_0" id="form_dataset_description_0" value="<?php echo $selected_dataset_description ?>"/></td>
+        <td><input class="text_inp size_dataset" type="text" name="dataset_<?php echo $row_num;?>" id="form_dataset_<?php echo $row_num;?>" value="<?php echo $selected_dataset ?>"/></td>
+        <td><input class="text_inp size_dataset" type="text" name="dataset_description_<?php echo $row_num;?>" id="form_dataset_description_<?php echo $row_num;?>" value="<?php echo $selected_dataset_description ?>"/></td>
       
         <td>
-        <select name="env_source_name_0" id="form_env_source_name_0">
+        <select name="env_source_name_<?php echo $row_num;?>" id="form_env_source_name_<?php echo $row_num;?>">
       
        <?php 
          print_options($env_source_names, $selected_env_source_name);        
@@ -74,7 +76,7 @@
       <?php 
       foreach ($arr_fields_add as &$value) {        
         echo '
-        <td><input class="text_inp size_tubelabel" type="text" name="'.$value.'_0" id="form_'.$value.'_0" value="'.${'selected_'.$value}.'"/></td>
+        <td><input class="text_inp size_tubelabel" type="text" name="'.$value.'_'.$row_num.'" id="form_'.$value.'_'.$row_num.'" value="'.${'selected_'.$value}.'"/></td>
         ';  
       }
       unset($value); // break the reference with the last element
