02ff9cf30f0a167709de1d2687c9f2d12b974fca
annaship <tetya-loshad@yandex.ru>
2013-03-28 11:30:21 -0400

    how to download csv from VAMPS

--- ill_subm_menu.php
+++ ill_subm_menu.php
@@ -1,14 +1,14 @@
 <div id="my_menu">
   <ul id="nav"> 
    
-<!--   <li><a href="step_subm_metadata.php">Create Submission Metadata</a></li> -->
-  <li><a href="#here">Submit Metadata</a>
-    <ul class="nav first">     
-      <li><a href="step_upload_subm_metadata.php">Upload Submission Metadata</a></li>
-      <li><a href="step_subm_metadata.php">Create Submission Metadata</a></li>
-      <li><a href="step_take_subm_metadata.php">Take Metadata from VAMPS</a></li>
-    </ul>  
-  </li>      
+  <li><a href="step_upload_subm_metadata.php">Upload Submission Metadata</a></li>
+<!--   <li><a href="#here">Submit Metadata</a> -->
+<!--     <ul class="nav first">      -->
+<!--       <li><a href="step_upload_subm_metadata.php">Upload Submission Metadata</a></li> -->
+<!--       <li><a href="step_subm_metadata.php">Create Submission Metadata</a></li> -->
+<!--       <li><a href="step_take_subm_metadata.php">Take Metadata from VAMPS</a></li> -->
+<!--     </ul>   -->
+<!--   </li>       -->
   
   <li><a href="step_overlap.php">Overlap Analyzing</a></li>
   <li><a href="step_gast.php">Gast</a></li>
--- step_upload_subm_metadata.php
+++ step_upload_subm_metadata.php
@@ -2,7 +2,10 @@
 <?php include_once "ill_subm_filled_variables.php"; ?>
 
 <h1>Illumina files processing</h1>
-<?php include_once("ill_subm_menu.php"); ?>
+<?php 
+  include_once("ill_subm_menu.php"); 
+  include_once("step_upload_csv_instruction.php");
+?>
 
 <div id = "csv_load">
 <form action="step_upload_subm_metadata.php" method="POST" enctype="multipart/form-data" name="form_upload_csv" id="form_upload_csv">
