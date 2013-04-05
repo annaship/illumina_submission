97a44d19172c9943db367d6b7d92710089f0b774
annaship <tetya-loshad@yandex.ru>
2013-04-05 18:20:10 -0400

    add lane and raw path to get metadata form

--- step_form_get_metadata.php
+++ step_form_get_metadata.php
@@ -27,7 +27,7 @@ print_out($_SERVER);
 </tr> 
 
 <tr class="fields">
-  <td class="fields" ><label for="find_machine">Run date</label>
+  <td class="fields" ><label for="find_machine">Machine name</label>
   </td>
   <td>
 	<select name="find_machine" id="form_machine">
@@ -37,6 +37,19 @@ print_out($_SERVER);
     </select>
   </td>
 </tr> 
+<tr class="fields">
+  <td class="fields" ><label for="find_lane">Lane number</label>
+  </td>
+  <td>
+  	<input class="text_inp size_abbr" type="text" name="find_lane" id="form_lane" value=""/>
+  </td>
+</tr> 
+<!-- Full path to raw data -->
+<tr class="fields">
+  <td class="fields" align="left"><label for="run_infoForm_path to raw data">path to raw data</label></td>
+  <td align="left"  colspan = 5><span class="emph">/xraid2-2/sequencing/Illumina/</span><input class="text_inp size_long_input" type="text" 
+      name="path to raw data" id="run_infoForm_path to raw data" value="<?php echo $selected_path_to_raw_data;?>"/></td>
+</tr>
 
 </tbody></table>
 </fieldset>
