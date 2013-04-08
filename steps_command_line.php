9500fcacd5bda2a71a8e39f118deb503eb9afcae
annaship <tetya-loshad@yandex.ru>
2013-04-08 13:25:15 -0400

    error message if no such csv

--- steps_command_line.php
+++ steps_command_line.php
@@ -78,20 +78,27 @@
 	          </p>
 	          <br/>
 	        ";
-	foreach ($lanes as $lane_name)
+	if ($csv_path_error)
 	{
-		print_red_message("lane_name = $lane_name");
-		$csv_name      = create_csv_name($rundate, $lane_name);
-		$csv_file_name =  $path_to_csv  . $rundate . "/" . $csv_name;
-		 
-		$command_line = "cd " . $path_to_csv . $rundate .
-		"; time python /bioware/linux/seqinfo/bin/python_pipeline/py_mbl_sequencing_pipeline/pipeline-ui.py
-	          -csv " . $path_to_csv  . $rundate . "/" . $csv_name .
-		          " -s " . $pipeline_command . " -l debug -p illumina -r " .
-		          $rundate . " -ft fastq -i " . $raw_path . " -cp " . $is_compressed . " -lane_name \"lane_" . $lane_name . "\" -do_perfect " . $do_perfect
-		          ;
-		           
-		print_red_message($command_line);
+		print_red_message($csv_path_error);
+	}
+	else
+	{
+		foreach ($lanes as $lane_name)
+		{
+			print_red_message("lane_name = $lane_name");
+			$csv_name      = create_csv_name($rundate, $lane_name);
+			$csv_file_name =  $path_to_csv  . $rundate . "/" . $csv_name;
+			 
+			$command_line = "cd " . $path_to_csv . $rundate .
+			"; time python /bioware/linux/seqinfo/bin/python_pipeline/py_mbl_sequencing_pipeline/pipeline-ui.py
+		          -csv " . $path_to_csv  . $rundate . "/" . $csv_name .
+			          " -s " . $pipeline_command . " -l debug -p illumina -r " .
+			          $rundate . " -ft fastq -i " . $raw_path . " -cp " . $is_compressed . " -lane_name \"lane_" . $lane_name . "\" -do_perfect " . $do_perfect
+			          ;
+			           
+			print_red_message($command_line);
+		}
 	}
 	echo "</div>";
 	
