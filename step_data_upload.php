e59cc7b962b81732045281e8914908235a5c5b42
annaship <tetya-loshad@yandex.ru>
2013-04-08 13:40:49 -0400

    add run_info adn data upload steps

--- step_data_upload.php
+++ step_data_upload.php
@@ -1,8 +1,21 @@
 <?php include_once("ill_subm_beginning.php"); ?>
+<?php
+  	include_once("ill_subm_functions.php");
+  	include_once "ill_subm_filled_variables.php";
+?>
        
       <h1>Illumina files processing</h1>
-      <?php include_once("ill_subm_menu.php"); ?>
+<?php 
+	include_once("ill_subm_menu.php");
 
+	$pipeline_command = "env454upload";
+	
+	include_once("steps_command_line.php");
+	
+
+?>
+	
+</div>
       
       <!-- end of content -->    
 <?php include_once("ill_subm_end.php"); ?>     
--- step_overlap.php
+++ step_overlap.php
@@ -11,22 +11,8 @@
 	$pipeline_command = "illumina_files";
 	
 	include_once("steps_command_line.php");
-	
-	print_red_message("From step_overlap!!!");
-	print_red_message("\$csv_file_name = $csv_file_name");
-	
-// 	echo "<div id=\"command_line_print\">";
-	
-// 	$csv_path_error
-// 	$command_line_overlap = "cd " . $path_to_csv . $rundate .
-// 	     	"; time python /bioware/linux/seqinfo/bin/python_pipeline/py_mbl_sequencing_pipeline/pipeline-ui.py
-//     	      -csv " . $csv_file_name .
-//               " -s illumina_files -l debug -p illumina -r " .
-//               $rundate . " -ft fastq -i " . $raw_path . " -cp " . $is_compressed . " -lane_name \"lane_" . $lane_name . "\" -do_perfect " . $do_perfect
-//                  ;
-// 	print_red_message($command_line_overlap);
+
 ?>
-<!-- 	</div> -->
 	
 </div>
       
