2a9214b32efacd872a36abfa308edf1e1d69adfe
annaship <tetya-loshad@yandex.ru>
2013-04-16 16:32:05 -0400

    remove local_conn

--- ill_subm_conn_local.php
+++
@@ -1,8 +0,0 @@
-<?php 
-  $local_mysqli = new mysqli("localhost", "root", "", "test");
-  if ($local_mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $local_mysqli->connect_errno . ") " . $local_mysqli->connect_error;
-  }
-  
-//   echo $local_mysqli->host_info . "<br/>from conn_local";
-?>
