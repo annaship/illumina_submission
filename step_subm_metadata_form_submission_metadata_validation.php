c8e3bf14801a83d14600a18d0a81dc2939352c62
annaship <tetya-loshad@yandex.ru>
2013-06-04 17:56:57 -0400

    merge w vampsdev step_subm_metadata_form_submission_metadata_validation.php

--- step_subm_metadata_form_submission_metadata_validation.php
+++ step_subm_metadata_form_submission_metadata_validation.php
@@ -8,9 +8,16 @@ include_once("ill_subm_functions.php");
 
 $metadata_errors     = array();
 $metadata_errors_all = array();
-// print_blue_message("\$_POST");
+$result_metadata_arr_checked = $selected_metadata_arr = array();
+$result_metadata_arr = separate_metadata($_POST, $arr_fields_headers);
 // print_out($_POST);
 
+// remove array #0 == check_submission
+if (sizeof($result_metadata_arr) > 1 && isset($_POST["submission_metadata_process"]) && ($_POST["submission_metadata_process"] == 1))
+{
+  $result_metadata_arr_0 = array_shift($result_metadata_arr);
+}
+  
 // TODO: 1) validate all data in foreach $result_metadata_arr
 // TODO: 2) populate index and runkey by adapter
 // TODO: 3) print out in table to show with errors in red and allow to change, 
@@ -27,15 +34,15 @@ array_push($required_fields, "funding", "project_description");
 //   print_blue_message("FROM valid1");
 //   print_out($result_metadata_arr);
 
-foreach ($combined_metadata as $combined_metadata_row)
+foreach ($result_metadata_arr as $result_metadata_arr1)
 {
-// 	print_blue_message("\$combined_metadata_row");
-// 	print_out($combined_metadata_row);
+// 	print_blue_message("\$result_metadata_arr1");
+// 	print_out($result_metadata_arr1);
 	
-  $metadata_errors = check_required_fields($combined_metadata_row, $required_fields);
+  $metadata_errors = check_required_fields($result_metadata_arr1, $required_fields);
   
   $field_name = "lane";
-  if( isset($combined_metadata_row[$field_name]) && !valid_is_number($combined_metadata_row[$field_name]))
+    if( isset($result_metadata_arr1[$field_name]) && !valid_is_number($result_metadata_arr1[$field_name]))
   {
     $metadata_errors[$field_name] = "The " . $field_name . " should be numbers.";
   }
@@ -43,7 +50,18 @@ foreach ($combined_metadata as $combined_metadata_row)
   
 
   // 2) populate index and runkey by adapter
+  if (check_var($_SESSION["run_info"]["dna_region_0"]))
+  {
+    $selected_dna_region_base = strtolower($_SESSION["run_info"]["dna_region_0"]);      
+  }
+  
+  $key_ind = get_run_key_by_adaptor($result_metadata_arr1, $adaptors_full, $selected_dna_region_base);
 
+
+  $result_metadata_arr1["run_key"]       = $key_ind["illumina_run_key"];
+  $result_metadata_arr1["barcode_index"] = $key_ind["illumina_index"];    
+  $selected_metadata_arr[] = $result_metadata_arr1;
+    
 }
 // check for errors
 $all_errors_uniq       = flat_mult_array($metadata_errors_all);
@@ -62,8 +80,6 @@ if ($error_field_names != "")
 	There is no data for: \"$error_field_names\". 
 	Probably the metadata were not properly submitted via <a href=\"http://vamps.mbl.edu/utils/submissions/project_submit.php\" target=\"_blank\">Project Submission</a> on VAMPS. 
 	<br/>
-	The \"submit_code\" to check is $current_submit_code.
-	<br/>
 	<br/>
 	");
 }
@@ -74,16 +90,13 @@ include_once "step_subm_metadata_form_metadata_table_selected.php";
 if($metadata_errors_count == 0)
 {
 // 	TODO: change env_id and data_owner 
-//   print_blue_message("\$result_metadata_arr");
-//   print_out($result_metadata_arr);
+  $result_metadata_arr = $selected_metadata_arr;
+
   //   put data into the db and clean the table
-// 	print_blue_message("FROM valid10");
-	
   include_once 'insert_metadata.php';
-//   print_blue_message("FROM valid11");
+//   print_blue_message("FROM valid1");
 //   print_out($result_metadata_arr);
   success_message('Metadata');
 }
 
 ?>
-
