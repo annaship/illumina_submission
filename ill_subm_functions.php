a57708305f3d278a39dd4a7f2ef4d9196d793dc0
annaship <tetya-loshad@yandex.ru>
2013-06-04 17:47:24 -0400

    temp

--- ill_subm_functions.php
+++ ill_subm_functions.php
@@ -545,6 +545,7 @@ function run_query($query, $table_name, $connection)
 function run_multi_query($multi_query, $connection)
 {
 	print_blue_message("HERE");
+	print_blue_out_message('$multi_query', $multi_query);
 	if (isset($_SESSION['is_local']) && !empty($_SESSION['is_local']))
 	{
 		$connection->multi_query($multi_query) or die("Multi query failed. The last error: (" . $connection->errno . ") " . $connection->error);
