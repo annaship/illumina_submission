662cde43b4f8baddfc5cf6847af8bcc03505be81
annaship <tetya-loshad@yandex.ru>
2013-07-24 14:32:41 -0400

    not not temporary table

--- insert_metadata.php
+++ insert_metadata.php
@@ -105,22 +105,22 @@
 // 	2) update the old one
 // 	$res = "";
 	$backup_subm_metadata_query1 = "";
-	$backup_subm_metadata_query1 = "CREATE TEMPORARY TABLE " . $db_name . ".tmptable_1 SELECT * FROM " . $db_name . ".vamps_submissions
+	$backup_subm_metadata_query1 = "CREATE TABLE " . $db_name . ".tmptable_1 SELECT * FROM " . $db_name . ".vamps_submissions
   	    WHERE submit_code	= \"" . $combined_metadata_row["submit_code"] . "\"
 			AND id 			= \"" . $combined_metadata_row["vamps_submissions_id"] . "\";
 		UPDATE " . $db_name . ".tmptable_1 SET submit_code = CONCAT(submit_code, \"_backup_\", \"" . date("Ymdhis") . "\");
 		UPDATE " . $db_name . ".tmptable_1 SET id = 0;				
 		INSERT IGNORE INTO " . $db_name . ".vamps_submissions SELECT * FROM " . $db_name . ".tmptable_1 LIMIT 1;
-		DROP TEMPORARY TABLE IF EXISTS " . $db_name . ".tmptable_1;
+		DROP TABLE IF EXISTS " . $db_name . ".tmptable_1;
 	";
 	$backup_subm_metadata_query2 = "";
-	$backup_subm_metadata_query2 = "CREATE TEMPORARY TABLE " . $db_name . ".tmptable_1 SELECT * FROM " . $db_name . ".vamps_submissions_tubes
+	$backup_subm_metadata_query2 = "CREATE TABLE " . $db_name . ".tmptable_1 SELECT * FROM " . $db_name . ".vamps_submissions_tubes
 		WHERE submit_code = \"" . $combined_metadata_row["submit_code"] . "\"
 			AND id = \"" . $combined_metadata_row["submissions_tubes_id"] . "\";
 		UPDATE " . $db_name . ".tmptable_1 SET submit_code = CONCAT(submit_code, \"_backup_\", \"" . date("Ymdhis") . "\");
 		UPDATE " . $db_name . ".tmptable_1 SET id = 0;
 		INSERT IGNORE INTO " . $db_name . ".vamps_submissions_tubes SELECT * FROM " . $db_name . ".tmptable_1 LIMIT 1;
-		DROP TEMPORARY TABLE IF EXISTS " . $db_name . ".tmptable_1;
+		DROP TABLE IF EXISTS " . $db_name . ".tmptable_1;
 	";
 	
 	array_push($all_backup_metadata_queries, $backup_subm_metadata_query1, $backup_subm_metadata_query2);
