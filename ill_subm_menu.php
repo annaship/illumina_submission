2e4a90a146b589affeb21c12598cba97afdd6eb1
annaship <tetya-loshad@yandex.ru>
2013-07-26 14:02:02 -0400

    add gzip to the main menu

--- ill_subm_menu.php
+++ ill_subm_menu.php
@@ -15,13 +15,15 @@
   <li><a href="step_gast.php">Gast</a></li>
   <li><a href="step_run_info_upload.php">Run_info upload</a></li>
   <li><a href="step_data_upload.php">Data upload</a></li>
-  <li><a href="#here">Optional steps</a>
+  <li><a href="step_gzip_all.php">gzip all</a></li>
+  <li><a href="#here">Separate steps</a>
   <ul class="nav first"> 
     <li><a href="step_demultiplex.php">demultiplex</a></li>
     <li><a href="step_overlap_only.php">overlap</a></li>
     <li><a href="step_filter_mismatch.php">filtering mismatch</a></li>
     <li><a href="step_uniqueing.php">uniqueing</a></li>
     <li><a href="step_chimera_checking_only.php">chimera checking</a></li>
+    <li><a href="step_gast.php">gast</a></li>
     <li><a href="step_clear_db.php">clear db</a></li>
     <li><a href="step_check_fa_counts.php">check counts in fa files</a></li>
     <li><a href="step_check_db_counts.php">check counts in db</a></li>
