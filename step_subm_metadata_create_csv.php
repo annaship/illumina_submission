9df771081e357ddb32a55d4c4d032947f5f202d6
annaship <tetya-loshad@yandex.ru>
2013-08-07 14:09:24 -0400

    data_all_dom

--- step_subm_metadata_create_csv.php
+++ step_subm_metadata_create_csv.php
@@ -41,76 +41,19 @@ foreach ($combined_metadata as $row_num => $combined_metadata_row)
 	$result_arr = array_intersect_key($combined_metadata_row, $table_headers_arr);
 // 	print_blue_message("\$result_arr - array_intersect_key = ");
 // 	print_out($result_arr);
+print_blue_out_message('$combined_metadata_row["domain"]', $combined_metadata_row["domain"]);
 	$domains[]	= $combined_metadata_row["domain"];
 	$lanes[]    = $combined_metadata_row["lane"];
 	$data_all[] = $result_arr;
+	$lane_dom  = $combined_metadata_row["domain"][0] . "_" . $combined_metadata_row["lane"];
+	$data_all_dom[$lane_dom][] = $result_arr;
 	
 }
-// print_blue_out_message('7) step_subm_metadata_create_csv, $combined_metadata', $combined_metadata);
 
-// {
-//   $contact_info   = array_map('trim', explode(',', $contact_full[$metadata_arr["user"]])); 
-// //   print_out($metadata_arr);
-  
-//   $data_for_csv["adaptor"] 		 	    = $metadata_arr["adaptor"];
-//   $data_for_csv["amp_operator"]         = $metadata_arr["amp_operator"];
-//   $data_for_csv["barcode"] 		        = $metadata_arr["barcode"];
-//   $data_for_csv["barcode_index"] 	    = $metadata_arr["barcode_index"];
-//   $data_for_csv["data_owner"]    	    = $metadata_arr["user"];
-//   $data_for_csv["dataset"] 	            = $metadata_arr["dataset"];
-//   if (check_var($metadata_arr["dataset_description"]))
-//   {	
-//   	$data_for_csv["dataset_description"]  = $metadata_arr["dataset_description"];
-//   }
-//   else
-//   {
-//   	$data_for_csv["dataset_description"]  = $metadata_arr["dataset"];
-//   }
-  	 
-//   $domains[]							= $metadata_arr["domain"];
-// //   $data_for_csv["domain"]        		= $metadata_arr["domain"];
-//   $data_for_csv["dna_region"] 	 	    = $metadata_arr["dna_region"];
-//   $data_for_csv["email"]         		= $metadata_arr["email"];
-//   $data_for_csv["env_sample_source_id"]	= $metadata_arr["env_sample_source_id"]; 
-//   $data_for_csv["first_name"]    		= $metadata_arr["first_name"];
-//   $data_for_csv["funding"]       		= $metadata_arr["funding"];
-//   $data_for_csv["insert_size"] 	        = $metadata_arr["insert_size"];
-//   $data_for_csv["institution"] 	        = $metadata_arr["institution"] ;
-//   $data_for_csv["lane"] 		 		= $metadata_arr["lane"];
-//   $data_for_csv["last_name"]     		= $contact_info[0];
-//   $data_for_csv["overlap"] 		 	    = $_SESSION["run_info"]["overlap"];
-//   $data_for_csv["primer_suite"]  		= get_primer_suite_name($_SESSION["run_info"]["dna_region_0"], $metadata_arr["domain"]);
-// //   $data_for_csv["primer_suite_id"] 	    = get_primer_suite_id($_SESSION["run_info"]["dna_region_0"], $metadata_arr["domain"], $db_name, $connection);
-//   $data_for_csv["project"] 		 	    = $metadata_arr["project"];
-//   $data_for_csv["project_description"]  = $metadata_arr["project_description"];
-//   if (isset($metadata_arr["project_title"]) && $metadata_arr["project_title"] != "")
-//   {
-//   	$data_for_csv["project_title"] 		= $metadata_arr["project_title"];
-//   }
-//   else
-//   {
-//   	$data_for_csv["project_title"] 		= $metadata_arr["project"];
-//   }  
-//   $data_for_csv["read_length"] 	 	    = $_SESSION["run_info"]["read_length"];
-//   $data_for_csv["run"]  		 		= $_SESSION["run_info"]["rundate"];
-//   $data_for_csv["run_key"] 	     	    = "NNNN" . $metadata_arr["run_key"];
-//   $data_for_csv["seq_operator"]  		= $_SESSION["run_info"]["seq_operator"];
-//   $data_for_csv["tube_label"] 	 		= $metadata_arr["tube_label"];
-
-//   $lanes[] = $data_for_csv["lane"];
-//   $data_all[] = $data_for_csv;
-// }
-
-// $csv_creat_errors = validate_data_for_csv($data_all);
-// print_blue_message("\$domains");
-// print_out($domains);
-
-
-
+// print_blue_out_message('$data_all before unshift', $data_all);
 array_unshift($data_all, $table_headers);
-$csv_data = array_to_scv($data_all, false);
-// TODO: create directory
-// -----
+// print_blue_out_message('$data_all after unshift', $data_all);
+// $csv_data = array_to_scv($data_all, false);
 $is_created = array();
 
 $_SESSION["run_info"]["lanes"] = array_unique($lanes);
