78a563f8893367dad52869ea5202263dfea586a5
annaship <tetya-loshad@yandex.ru>
2015-02-19 10:36:50 -0500

    remove print outs

--- ill_subm_functions.php
+++ ill_subm_functions.php
@@ -1392,12 +1392,7 @@ function get_primer_suite_name_from_db($data_arr, $connection)
 	 		run = \"" . $rundate . "\"
 	 		AND lane = " . $lane . "			
 				";
-		print_blue_out_message('FUNC1: $domain = ', $domain);
 		$first4let = substr($domain, 0, 4); 
-		print_blue_out_message('FUNC2: $first4let = ', $first4let);
-		
-// 		print_blue_out_message('FUNC1: $query = ', $query);
-// 		print_blue_out_message('FUNC1: $first4let = ', $first4let);
 		// 		$suite_names[] = get_one_value($query, $db_name, $connection);
 		$suite_names[] = run_query_and_get_all($query, $connection);		
 	}
@@ -1409,16 +1404,12 @@ function get_primer_suite_name_from_db($data_arr, $connection)
 				
 			if ((startsWith($p_suite[primer_suite], $first4let)) || strtolower($domain) == strtolower("its1"))
 			{
-			print_blue_out_message('FUNC3: $p_suite = ', $p_suite);
-			print_blue_out_message('$p_suite[primer_suite] = ', $p_suite[primer_suite]);
 				$suite_name_lane_domain[$lane][$p_suite["dna_region"]] = $p_suite;
 // 				$suite_name = $p_suite[primer_suite];
 			}
 		}
 	}
 	
-// 	print_blue_out_message('$suite_name', $suite_name);
-	print_blue_out_message('$suite_name_lane_domain', $suite_name_lane_domain);
 	return $suite_name_lane_domain;
 }
 
