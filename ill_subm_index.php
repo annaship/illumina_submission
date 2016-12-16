edd1332e5dc503154e89f92dcb3438fcfdd6c458
annaship <ashipunova@as-macbook.local>
2016-12-16 18:40:10 -0500

    redirect_message.php

--- ill_subm_index.php
+++ ill_subm_index.php
@@ -1,7 +1,10 @@
 <?php include_once("ill_subm_beginning.php"); ?>
        
       <h1>Illumina files processing</h1>
-      <?php include_once("ill_subm_menu.php"); ?>
+      <?php 
+      // include_once("ill_subm_menu.php"); 
+      include_once("redirect_message.php"); 
+      ?>
 
       
       <!-- end of content -->    
--- ill_subm_index_local.php
+++ ill_subm_index_local.php
@@ -1,7 +1,9 @@
 <?php 
 error_reporting(E_ERROR | E_PARSE);
 include_once("ill_subm_beginning.php"); 
-  include_once("ill_subm_menu.php"); 
+  // include_once("ill_subm_menu.php"); 
+  include_once("redirect_message.php"); 
+  
 ?>
       
       <!-- end of content -->    
